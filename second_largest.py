55cb69f7f25d28790866cfb26ccd4aacd27abdf0
Bushra <aayatkhabam391@gmail.com>
2025-10-14 21:51:25 +0530

    Update second_largest.py

--- second_largest.py
+++ second_largest.py
@@ -1,3 +1,21 @@
+# Given the scores for a set of participants, find the score of the runner-up.
+
+# The scores may include duplicates.
+
+# The runner-up is defined as the second highest unique score in the list.
+
+# Input Format
+# The first line contains an integer, n, the number of scores.
+
+# The second line contains an array of n integers separated by a space, representing the scores.
+
+# Constraints
+# 2 ≤ n ≤ 10
+# −100 ≤ A[i] ≤ 100 (Note: The scores can be negative)
+
+# Hence Need to find second largest
+
+
 n = int(input())
 arr = list(map(int, input().split()))
 
